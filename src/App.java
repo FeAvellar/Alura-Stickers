51377b534d9f30ad19a913f821776043510c37e5
FeAvellar <fe.avellar91@gmail.com>
2023-03-28 14:27:22 -0300

    Desafio dia 1 - Estrelas/Personalização

--- src/App.java
+++ src/App.java
@@ -15,7 +15,7 @@ public class App {
         
 // Passo 1 - Fazer conexão http (conectar com o site)e buscar os Top 250 filmes
 
-String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json"; //Alternativo
+String url = "https://raw.githubusercontent.com/lukadev08/lukadev08.github.io/main/apidata/imdbtop250moviesdata.json"; //Alternativo
 URI endereco = URI.create(url);
 var client = HttpClient.newHttpClient();
 HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
@@ -31,14 +31,19 @@ System.out.println(body);
 
 var parser = new JsonParser();  //Utilizado para separar partes do texto (parsear)
 List<Map<String, String>> listaDeFilmes = parser.parse(body);
-// System.out.println(listaDeFilmes.size()); ---> Size serve para enumerar a qtd de informações
+//System.out.println(listaDeFilmes.size()); ---> Size serve para enumerar a qtd de informações
 
-// Passo 3 - Exibir e manipular os dados na ide
+//Passo 3 - Exibir e manipular os dados na ide
 for (Map<String,String> filme : listaDeFilmes) { //for --> Para essa lista eu quero que imprima as informações que descrevi abaixo
-    System.out.println(filme.get("title"));
-    System.out.println(filme.get("image"));
-    System.out.println(filme.get("imDbRating"));
-   
+    System.out.println("\n\nFilme: " + "\u001b[1m" +filme.get("title") + "\u001b[m");
+    System.out.println("Link da Imagem: " + "\u001b[4m" + filme.get("image") + "\u001b[m");
+    
+    double classificacao = Double.parseDouble(filme.get("imDbRating"));
+    int numeroEstrelas = (int) classificacao;
+    for (int n = 1; n <= numeroEstrelas; n++){
+        System.out.print("⭐️") ;
+    }
+    System.out.println("\u001b[46;1m \u001b[37m" + "Nota: " +  filme.get("imDbRating") + "\u001b[m");
 
        }
     }
